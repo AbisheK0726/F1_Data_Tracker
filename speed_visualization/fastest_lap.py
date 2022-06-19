8b4462beb14fe8ce807df5c3b0fb7a52d30dae42
AbisheK0726 <abishekaneese726@gmail.com>
2022-06-19 04:23:02 +0100

    Testing the functionality of numpy.reshape and numpy.concatenate

--- speed_visualization/fastest_lap.py
+++ speed_visualization/fastest_lap.py
@@ -16,24 +16,28 @@ def select_driver_lap_fastest(driver,session):
     lap = session.laps.pick_driver(driver).pick_fastest()
     return lap
 
-def get_position_data(lap):
-    """ Returns the data for the position on the track """
-    x_pos = lap.telemetry['X']      # values for x-axis
-    y_pos = lap.telemetry['Y']      # values for y-axis
-
 def main():
     print("Visualistion of the speed on the track for a given driver", end="\n\n")
 
     colormap = mpl.cm.plasma    
     grand_prix = "Sao Paulo Grand Prix" #input("Select a Grand Prix: ")
-    year = int(input("Select a year: "))
-    driver = input("Select a driver: ")
-    session = input("Select a session, (FP1, FP2, FP3, Q1, Q2, Q3, R): ")
+    year = 2021                         #int(input("Select a year: "))
+    driver = "HAM"                      #input("Select a driver: ")
+    session = "R"                       #input("Select a session, (FP1, FP2, FP3, Q1, Q2, Q3, R): ")
 
     session = ff1.get_session(year, grand_prix, session)
     session.load()
 
     lap = select_driver_lap_fastest(driver,session)
 
+    x_pos = lap.telemetry['X']      # values for x-axis
+    y_pos = lap.telemetry['Y'] 
+    tack_color = lap.telemetry['Speed']
+    
+    
+    pos_array = np.array([x_pos, y_pos]) #Create a numpy array of the positions
+    points = pos_array.T.reshape(-1, 1, 2) #Reshape the array to be (numlines) x (points per line) x 2 (for x and y)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
 main()
+
